7a785e6e63e20f723af7f26f60614d82c6aeaf23
frol <mail@frolenkodima.ru>
2023-08-13 15:42:10 +0300

    yandex cloud support

---
+++ index.go
@@ -0,0 +1,26 @@
+//This file used for cloud function in yandex.cloud
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+)
+
+func Handler(ctx context.Context) (*Response, error) {
+	conn := Connect()
+	defer conn.Close(context.Background())
+	videos := conn.GetVideos()
+
+	r := ResponseBody{List: videos}
+	body, err := json.Marshal(&r)
+	if err != nil {
+		println("Error marshalling json")
+		panic(`json error`)
+	}
+	fmt.Println(string(body))
+	resp := Response{StatusCode: 200, Body: string(body)}
+
+	return &resp, nil
+}
